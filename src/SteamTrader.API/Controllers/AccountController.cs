11b09d6cb5603777a7519a457e79aa820ece3575
Yury <66903760+bezlla@users.noreply.github.com>
2022-01-17 19:58:21 +0300

    Refactoring

---
+++ src/SteamTrader.API/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamTrader.API.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+        //TODO: Register Account 
+    }
+}
---
+++ src/SteamTrader.API/Controllers/SteamGuardController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamTrader.API.Controllers
+{
+    public class SteamGuardController : ControllerBase
+    {
+        //TODO: Register SteamGuard
+    }
+}
