92c825c80ff00b98e3b6ac7d67b1bab60a7410fa
y.fomenkov <y.fomenkov>
2022-01-25 04:36:00 +0300

    Added migrations

---
+++ src/SteamTrader.Infrastructure.Data/Migrations/20220125013504_AddName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SteamTrader.Infrastructure.Data.Migrations
+{
+    public partial class AddName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "TradeOffers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "TradeOffers");
+        }
+    }
+}
